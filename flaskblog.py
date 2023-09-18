29bc6e25e6b8c5972e7453a2e64a48e491c03258
erdoganb <erdoganborklu@gmail.com>
2023-09-18 14:48:55 +0300

    forms and input

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '02d239d851055e158364a8087a7fb8b4'
 
 posts = [
     {
@@ -19,13 +21,34 @@ posts = [
 #home page
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.html', title="home", posts=posts)
 
 #about page
 @app.route("/about")
 def about():
     return render_template('about.html', title="about")
+
+#registration page
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title="register", form=form)
+
+#login page
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "a@a.a" and form.password.data=="ccc":
+            flash("You've been logged in!", "success")
+            return redirect(url_for('home'))
+        else:
+            flash("Login unsuccessful. Please check your mail and password!", "danger")
+    return render_template('login.html', title="register", form=form)
 
 
 
