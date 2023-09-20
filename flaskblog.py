51e5b05f1280e5c81182af57643dc3f36aa06f6f
erdoganb <erdoganborklu@gmail.com>
2023-09-20 09:22:42 +0300

    added database feature

--- flaskblog.py
+++ flaskblog.py
@@ -2,10 +2,40 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 import json
-
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '02d239d851055e158364a8087a7fb8b4'
+##DATABASE
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///site.db"
+db = SQLAlchemy(app)
+#DATABASE MODELS
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False) #passwords will be hashed!
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}'), '{self.email}', '{self.email}', '{self.image_file}'"
+
+class Post(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=True)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)##remove nullable! due to an error
+    ##datetime.utcnow is a function!
+    ##datetime.utcnow() return of a function!
+    content = db.Column(db.Text, nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))##remove nullable! due to an error
+#error was-> TypeError: Additional arguments should be named <dialectname>_<argument>, got 'nullabe'
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
 
 # Function to parse JSON file
 def parse_json_file(file_path):
@@ -55,4 +85,4 @@ def login():
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
