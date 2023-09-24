24260b9c79619b342962f9fb58e196550de10c4a
erdoganb <erdoganborklu@gmail.com>
2023-09-24 21:34:04 +0300

    profile picture update

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,9 +1,12 @@
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog.models import User, Post
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 import json
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
+from PIL import Image
 
 # Function to parse JSON file
 def parse_json_file(file_path):
@@ -69,9 +72,35 @@ def logout():
     flash("Logout ok!", 'danger')
     return redirect(url_for('home'))
 
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
 
 #account
-@app.route("/account")
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='account', image_file=image_file, form=form)
--- run.py
+++ run.py
@@ -3,4 +3,4 @@ from flaskblog import app
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
